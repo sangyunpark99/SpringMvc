facdeb04df229e10f08d0520bf1673383d8de83b
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-06-10 08:20:55 +0900

    HTTP 요청 - 기본, 헤더 조회

--- src/main/java/hello/springmvc/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/requestmapping/MappingClassController.java
@@ -5,32 +5,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String user(){
         return "users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping("/userId")
     public String addUser(){
         return "post user";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId){
         return "get " + userId;
     }
 
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId){
         return "update " + userId;
     }
 
-    @DeleteMapping("/mapping/users/{userId")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
         return "delete " + userId;
     }
